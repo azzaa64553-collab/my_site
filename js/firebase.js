8081780de398d50d5e54d76e8b2a66a7bca41dd1
azzaa64553-collab <azzaa64553@gmail.com>
2026-07-29 21:08:18 +0330

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,17 +1,35 @@
-// Firebase Config
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 
-export const firebaseConfig = {
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
-    apiKey: "",
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-storage.js";
 
-    authDomain: "",
+// Config
+const firebaseConfig = {
 
-    projectId: "",
+    apiKey: "AIzaSyBM5zOgIQKSG7_zQJ_L7taB0CQGjYWRSVA",
 
-    storageBucket: "",
+    authDomain: "la3chat.firebaseapp.com",
 
-    messagingSenderId: "",
+    projectId: "la3chat",
 
-    appId: ""
+    storageBucket: "la3chat.firebasestorage.app",
+
+    messagingSenderId: "831502350386",
+
+    appId: "1:831502350386:web:944a347e3b7656ea60242b"
 
 };
+
+// Initialize
+const app = initializeApp(firebaseConfig);
+
+// Firestore
+const db = getFirestore(app);
+
+// Storage
+const storage = getStorage(app);
+
+// Export
+export { app, db, storage };
